0e943c13579778b38eb9ef5fe5e2d79be29a1838
Muhammad Arslan <gery_maz_555@hotmail.com>
2026-05-10 11:30:40 +0500

    dataBinding

--- app/src/main/java/com/mhdarslan/room_database_kotlin/MainActivity.kt
+++ app/src/main/java/com/mhdarslan/room_database_kotlin/MainActivity.kt
@@ -3,13 +3,15 @@ package com.mhdarslan.room_database_kotlin
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.mhdarslan.room_database_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
